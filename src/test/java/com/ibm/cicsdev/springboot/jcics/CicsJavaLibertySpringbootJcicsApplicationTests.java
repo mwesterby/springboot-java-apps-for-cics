77c49f4a97f19b7e3fa02b8661367305f1668d23
Michael Westerby <michael.westerby@ibm.com>
2020-10-14 10:48:27 +0100

    Initial Spring Initializr app

---
+++ src/test/java/com/ibm/cicsdev/springboot/jcics/CicsJavaLibertySpringbootJcicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.cicsdev.springboot.jcics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicsJavaLibertySpringbootJcicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
